e2af7665a8d8002db8cdc9548a50b017416b68fd
Oles <oles@olestourko.ca>
2017-02-25 21:59:26 -0500

    fleshed out the UI a bit

--- src/frontend/frontend.py
+++ src/frontend/frontend.py
@@ -1,18 +1,74 @@
 #!/usr/bin/env python
 import curses
+import time
+
+screen = None
+
+def draw_screen(erase=True):
+	global screen
+
+	if screen is None:
+		screen = curses.initscr()
+		curses.noecho()
+		curses.cbreak()
+		curses.curs_set(0)
+
+	if erase:
+		screen.erase()
+
+	screen.addstr(0, 0, "[R] Revenue", curses.A_NORMAL)
+	screen.addstr(1, 0, "[E] Expenses", curses.A_NORMAL)
+	screen.addstr(2, 0, "[A] Adjustments", curses.A_NORMAL)
+	screen.addstr(4, 0, "[N] Net Income Target", curses.A_NORMAL)
+	screen.addstr(5, 0, "[L] Last Account Balance", curses.A_NORMAL)
+	screen.addstr(7, 0, "[X] Exit", curses.A_NORMAL)
+	screen.addstr(screen.getmaxyx()[0]-1, 0, "Command:", curses.A_REVERSE)
+	screen.refresh()
 
 if __name__ == "__main__":
-	screen = curses.initscr()
+	draw_screen()
 
-	curses.noecho()
-	curses.cbreak()
-	curses.curs_set(0)
+	while True:
+		ch = chr(screen.getch())
+		maxyx = screen.getmaxyx()
+		screen.addch(maxyx[0]-1, 9, ch)
+		screen.refresh()
 
-	screen.addstr("$2000.0", curses.A_NORMAL)
-	screen.chgat(-1, curses.A_NORMAL)
-	screen.addstr(screen.getmaxyx()[0]-1, 0, "Enter anything:", curses.A_REVERSE)
+		if ch == 'R':
+			curses.echo()
+			item = "Revenue"
+			screen.addstr(maxyx[0]-1, 0, "Enter {}:".format(item), curses.A_REVERSE)
+			value = screen.getstr(maxyx[0]-1, len(item) + 8)
+			curses.noecho()
+		elif ch == 'E':
+			curses.echo()
+			item = "Expenses"
+			screen.addstr(maxyx[0]-1, 0, "Enter {}:".format(item), curses.A_REVERSE)
+			value = screen.getstr(maxyx[0]-1, len(item) + 8)
+			curses.noecho()
+		elif ch == 'A':
+			curses.echo()
+			item = "Adjustments"
+			screen.addstr(maxyx[0]-1, 0, "Enter {}:".format(item), curses.A_REVERSE)
+			value = screen.getstr(maxyx[0]-1, len(item) + 8)
+			curses.noecho()
+		elif ch == 'N':
+			curses.echo()
+			item = "Net Income Target"
+			screen.addstr(maxyx[0]-1, 0, "Enter {}:".format(item), curses.A_REVERSE)
+			value = screen.getstr(maxyx[0]-1, len(item) + 8)
+			curses.noecho()
+		elif ch == 'L':
+			curses.echo()
+			item = "Last Account Balance"
+			screen.addstr(maxyx[0]-1, 0, "Enter {}:".format(item), curses.A_REVERSE)
+			value = screen.getstr(maxyx[0]-1, len(item) + 8)
+			curses.noecho()
+		elif ch == 'X':
+			break
+		else:
+			break
 
-	screen.refresh()
-	screen.getch()
+		draw_screen()
 
 	curses.endwin()
