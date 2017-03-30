b649c6ef97c0912941639fc917372629170478bd
Oles <oles@olestourko.ca>
2017-03-29 20:03:47 -0400

    Created migration for Month -> BudgetItem association tables, fixed JOOQ schema param

---
+++ java-production/src/main/resources/db/migration/V3__association_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE sdbudget.month_revenues (
+    id INT NOT NULL,
+    month_id INT NOT NULL,
+    budget_item_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (month_id) REFERENCES month(id),
+    FOREIGN KEY (budget_item_id) REFERENCES budget_item(id)
+);
+
+CREATE TABLE sdbudget.month_expenses (
+    id INT NOT NULL,
+    month_id INT NOT NULL,
+    budget_item_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (month_id) REFERENCES month(id),
+    FOREIGN KEY (budget_item_id) REFERENCES budget_item(id)
+);
+
+CREATE TABLE sdbudget.month_adjustments (
+    id INT NOT NULL,
+    month_id INT NOT NULL,
+    budget_item_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (month_id) REFERENCES month(id),
+    FOREIGN KEY (budget_item_id) REFERENCES budget_item(id)
+);
